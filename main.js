105cc7821a4677a2381724085dd13f8b8b9ae8dc
Dongja <jdh981118@naver.com>
2023-04-19 00:11:45 +0900

    js: arrow up

--- main.js
+++ main.js
@@ -23,10 +23,22 @@ homeContactBtn.addEventListener('click', () => {
 	scrollIntoView('#contact');
 });
 
+//make home slowly fade to transparents as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
 	home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+	if (window.scrollY > homeHeight / 2) {
+		arrowUp.classList.add('visible');
+	} else arrowUp.classList.remove('visible');
+});
+arrowUp.addEventListener('click', () => {
+	scrollIntoView('#home');
 });
 
 function scrollIntoView(selector) {
