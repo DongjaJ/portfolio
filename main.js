d6285722373bf2bc0d825c6749fe534d67c53d04
Dongja <jdh981118@naver.com>
2023-04-18 23:35:38 +0900

    js: home contact btn

--- main.js
+++ main.js
@@ -8,23 +8,21 @@ document.addEventListener('scroll', () => {
 	else navbar.classList.remove('navbar--dark');
 });
 
-// const navbarItems = Array.from(
-// 	document.querySelectorAll('.navbar__menu__item')
-// );
-// navbarItems.forEach((navbarItem) => {
-// 	navbarItem.addEventListener('click', () => {
-// 		const category = navbarItem.textContent.toLowerCase();
-// 		const targetSection = document.querySelector(`#${category}`);
-// 		console.log(category, targetSection);
-// 		targetSection.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
-// 	});
-// });
-
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (e) => {
 	const target = e.target;
 	const link = target.dataset.link;
 	if (link == null) return;
-	const scrollTo = document.querySelector(link);
-	scrollTo.scrollIntoView({ behavior: 'smooth' });
+	scrollIntoView(link);
 });
+
+//Handle click on 'contact me' button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+	scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+	const scrollTo = document.querySelector(selector);
+	scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
